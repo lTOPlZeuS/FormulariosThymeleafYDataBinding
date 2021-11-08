63f5dd66cc5a27a3729c538183a78ae3ba65c242
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-08 15:07:23 -0600

    Agregando validacion de no null a los inputs

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,17 +1,23 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
   @GetMapping("/form")
   public String form(Model model) {
+    Usuario usuario = new Usuario();
+    model.addAttribute("usuario", usuario);
     model.addAttribute("titulo", "Formulario");
     return "form";
   }
@@ -30,8 +36,16 @@ public class FormController {
 
   //Asignando valor por referencia a un objeto de manera dinamica y mas corta de la manera anterior
   @PostMapping("/form")
-  public String procesar(Usuario usuario, Model model) {
+  public String procesar(@Validated Usuario usuario, BindingResult result, Model model) {
     model.addAttribute("titulo", "Formulario");
+    if (result.hasErrors()) {
+      Map<String, String> errors = new HashMap<>();
+      result.getFieldErrors().forEach(error -> {
+        errors.put(error.getField(), error.getDefaultMessage());
+      });
+      model.addAttribute("errors", errors);
+      return "form";
+    }
     model.addAttribute("usuario", usuario);
     return "resultado";
   }
--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
+  @NotEmpty
   private String username;
+  @NotEmpty
   private String email;
+  @NotEmpty
   private String password;
 
 
