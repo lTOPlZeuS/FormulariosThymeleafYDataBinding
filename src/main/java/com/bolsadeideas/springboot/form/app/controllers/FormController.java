504fda929d7692da02e34a75bfb42b20fc2b2740
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-13 13:48:58 -0600

    Agregando validaciones

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,15 +1,16 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -17,12 +18,20 @@ import org.springframework.web.bind.support.SessionStatus;
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
+  @Autowired
+  private UsuarioValidador usuarioValidador;
+
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.addValidators(usuarioValidador);
+  }
+
   @GetMapping("/form")
   public String form(Model model) {
     Usuario usuario = new Usuario();
     usuario.setNombre("Juan");
     usuario.setApellido("Perez");
-    usuario.setIdentificador("122_212_666F");
+    usuario.setIdentificador("122.212.666-F");
     model.addAttribute("usuario", usuario);
     model.addAttribute("titulo", "Formulario");
     return "form";
@@ -43,6 +52,7 @@ public class FormController {
   //Asignando valor por referencia a un objeto de manera dinamica y mas corta de la manera anterior
   @PostMapping("/form")
   public String procesar(@Validated Usuario usuario, BindingResult result, Model model,SessionStatus status) {
+    // usuarioValidador.validate(usuario, result);
     model.addAttribute("titulo", "Formulario");
     if (result.hasErrors()) {
       // Manera larga de validar errores
@@ -54,7 +64,7 @@ public class FormController {
       return "form";
     }
     model.addAttribute("usuario", usuario);
-    status.setComplete();
+    status.setComplete(); 
     return "resultado";
   }
 }
